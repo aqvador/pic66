4afd7fddad1be4223bf09a9dca6f1295b0d7f996
neverzoom <neverzoom22@gmail.com>
2019-11-15 18:36:03 +0300

    index page, styles, fonts, images

--- views/site/index.php
+++ views/site/index.php
@@ -5,4 +5,41 @@
 $this->title = 'My Yii Application';
 ?>
 
-<h1>Hello, world!</h1>
+<section class="section">
+		<div class="container">
+			<div class="row main">
+				<div class="col-md-12 col-lg-12 col-xl-12">
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/photo.jpg" alt="photo">
+						<p>Печать ваших фото до формата А3</p>
+					</div>
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/cup.jpg" alt="cup">
+						<p>Кружки</p>
+					</div>
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/souvenirs.jpg" alt="souvenirs">
+						<p>Фотосувениры</p>
+					</div>
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/photo.jpg" alt="photo">
+						<p>Магниты</p>
+					</div>
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/cup.jpg" alt="cup">
+						<p>Металлические сувениры</p>
+					</div>
+					<div class="product">
+						<div class="lay"></div>
+						<img src="/img/souvenirs.jpg" alt="souvenirs">
+						<p>Печать на футболках</p>
+					</div>
+					
+				</div>
+			</div>
+		</section>
